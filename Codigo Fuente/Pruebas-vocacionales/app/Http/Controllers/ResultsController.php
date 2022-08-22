97c53dafc77cee0d98bc1b8ffb9ff7ab823f95f9
FelixPalacios117 <00055419@uca.edu.sv>
2022-08-21 23:52:23 -0600

    Plantilla de validez agregada

--- Codigo Fuente/Pruebas-vocacionales/app/Http/Controllers/ResultsController.php
+++ Codigo Fuente/Pruebas-vocacionales/app/Http/Controllers/ResultsController.php
@@ -41,77 +41,113 @@ class ResultsController extends Controller
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T'
         );
+        //Columna 12
         $validez = self::validez($respuestas);
         //dd($validez);
         return view('admin.resultados', compact('respuestas', 'actividades', 'validez'));
     }
-    public function validez($respuestas)
+    public function validez($respuestas)//evalua la plantilla de validez se auxilia de la función de casos
     {
-        $validez = 0;
-        //vamos validando uno por uno cada punto especifico por columna están del 12 al 1
-        if (substr($respuestas[0]->bloque_tres, 0, 1) == '+') {
-            $validez++;
-        }
-        if (substr($respuestas[0]->bloque_tres, 1, 1) == '+') {
-            $validez++;
-        }
-        if (substr($respuestas[0]->bloque_ocho, 0, 1) == '+') {
-            $validez++;
-        }
-        if (substr($respuestas[0]->bloque_ocho, 1, 1) == '-') {
-            $validez++;
-        }
-        if (substr($respuestas[0]->bloque_ocho, 2, 1) == '+') {
-            $validez++;
-        }
-        if (substr($respuestas[0]->bloque_doce, 0, 1) == '+') {
-            $validez++;
-        }
-        if (substr($respuestas[0]->bloque_doce, 1, 1) == '-') {
-            $validez++;
-        }
-        if (substr($respuestas[0]->bloque_doce, 2, 1) == '-') {
-            $validez++;
-        }
-        // Fin de la columna 12
-        // Inicio columna 11
-        if (substr($respuestas[1]->bloque_dos, 0, 1) == '+') {
-            $validez++;
-        }
-        if (substr($respuestas[1]->bloque_dos, 1, 1) == '-') {
-            $validez++;
-        }
-        if (substr($respuestas[1]->bloque_dos, 2, 1) == '-' || substr($respuestas[1]->bloque_dos, 2, 1) == '+') {
-            $validez++;
-        }
-        if (substr($respuestas[1]->bloque_cinco, 0, 1) == '-') {
-            $validez++;
-        }
-        if (substr($respuestas[1]->bloque_cinco, 1, 1) == '+') {
-            $validez++;
-        }
-        if (substr($respuestas[1]->bloque_cinco, 2, 1) == '-' || substr($respuestas[1]->bloque_cinco, 2, 1) == '+') {
-            $validez++;
-        }
-        if (substr($respuestas[1]->bloque_ocho, 0, 1) == '-') {
-            $validez++;
-        }
-        if (substr($respuestas[1]->bloque_ocho, 1, 1) == '+') {
-            $validez++;
-        }
-        if (substr($respuestas[1]->bloque_ocho, 2, 1) == '+') {
-            $validez++;
-        }
-        if (substr($respuestas[1]->bloque_trece, 0, 1) == '-' || substr($respuestas[1]->bloque_trece, 0, 1) == '+') {
-            $validez++;
-        }
-        if (substr($respuestas[1]->bloque_trece, 1, 1) == '-') {
-            $validez++;
-        }
-        if (substr($respuestas[1]->bloque_trece, 2, 1) == '+') {
-            $validez++;
-        }
-        //fin columna 11
+        //Columna 12
+        $validez = self::evaluar(substr($respuestas[0]->bloque_tres, 0, 1), 2);
+        $validez += self::evaluar(substr($respuestas[0]->bloque_tres, 1, 1), 2);
+        $validez += self::evaluar(substr($respuestas[0]->bloque_ocho, 0, 1), 2);
+        $validez += self::evaluar(substr($respuestas[0]->bloque_ocho, 1, 1), 1);
+        $validez += self::evaluar(substr($respuestas[0]->bloque_ocho, 2, 1), 2);
+        $validez += self::evaluar(substr($respuestas[0]->bloque_doce, 0, 1), 2);
+        $validez += self::evaluar(substr($respuestas[0]->bloque_doce, 1, 1), 1);
+        $validez += self::evaluar(substr($respuestas[0]->bloque_doce, 2, 1), 1);
+        //Columna 11
+        $validez += self::evaluar(substr($respuestas[1]->bloque_dos, 0, 1), 2);
+        $validez += self::evaluar(substr($respuestas[1]->bloque_dos, 1, 1), 1);
+        $validez += self::evaluar(substr($respuestas[1]->bloque_dos, 2, 1), 3);
+        $validez += self::evaluar(substr($respuestas[1]->bloque_cinco, 0, 1), 1);
+        $validez += self::evaluar(substr($respuestas[1]->bloque_cinco, 1, 1), 2);
+        $validez += self::evaluar(substr($respuestas[1]->bloque_cinco, 2, 1), 3);
+        $validez += self::evaluar(substr($respuestas[1]->bloque_ocho, 0, 1), 1);
+        $validez += self::evaluar(substr($respuestas[1]->bloque_ocho, 1, 1), 2);
+        $validez += self::evaluar(substr($respuestas[1]->bloque_ocho, 2, 1), 2);
+        $validez += self::evaluar(substr($respuestas[1]->bloque_trece, 0, 1), 3);
+        $validez += self::evaluar(substr($respuestas[1]->bloque_trece, 1, 1), 1);
+        $validez += self::evaluar(substr($respuestas[1]->bloque_trece, 2, 1), 2);
+        //Columna 10
+        $validez += self::evaluar(substr($respuestas[2]->bloque_uno, 0, 1), 2);
+        $validez += self::evaluar(substr($respuestas[2]->bloque_uno, 1, 1), 2);
+        //Columna 9
+        $validez += self::evaluar(substr($respuestas[3]->bloque_cinco, 0, 1), 2);
+        $validez += self::evaluar(substr($respuestas[3]->bloque_cinco, 1, 1), 1);
+        $validez += self::evaluar(substr($respuestas[3]->bloque_cinco, 2, 1), 1);
+        $validez += self::evaluar(substr($respuestas[3]->bloque_ocho, 0, 1), 2);
+        $validez += self::evaluar(substr($respuestas[3]->bloque_ocho, 1, 1), 2);
+        $validez += self::evaluar(substr($respuestas[3]->bloque_ocho, 2, 1), 1);
+        $validez += self::evaluar(substr($respuestas[3]->bloque_doce, 0, 1), 1);
+        $validez += self::evaluar(substr($respuestas[3]->bloque_doce, 1, 1), 1);
+        $validez += self::evaluar(substr($respuestas[3]->bloque_doce, 2, 1), 2); 
+        //Columna 8
+        $validez += self::evaluar(substr($respuestas[4]->bloque_seis, 0, 1), 1);
+        $validez += self::evaluar(substr($respuestas[4]->bloque_seis, 1, 1), 1);
+        $validez += self::evaluar(substr($respuestas[4]->bloque_seis, 2, 1), 2); 
+        $validez += self::evaluar(substr($respuestas[4]->bloque_doce, 0, 1), 2);
+        $validez += self::evaluar(substr($respuestas[4]->bloque_doce, 1, 1), 1);
+        $validez += self::evaluar(substr($respuestas[4]->bloque_doce, 2, 1), 1); 
+        //Columna 7
+        $validez += self::evaluar(substr($respuestas[5]->bloque_dos, 2, 1), 1); 
+        $validez += self::evaluar(substr($respuestas[5]->bloque_dos, 0, 1), 1);
+        $validez += self::evaluar(substr($respuestas[5]->bloque_diez, 0, 1), 2);
+        $validez += self::evaluar(substr($respuestas[5]->bloque_diez, 2, 1), 2); 
+        //Columna 6
+        $validez += self::evaluar(substr($respuestas[6]->bloque_dos, 0, 1), 1); 
+        $validez += self::evaluar(substr($respuestas[6]->bloque_dos, 1, 1), 1); 
+        $validez += self::evaluar(substr($respuestas[6]->bloque_dos, 2, 1), 2); 
+        //Columna 5
+        $validez += self::evaluar(substr($respuestas[7]->bloque_cuatro, 1, 1), 1); 
+        $validez += self::evaluar(substr($respuestas[7]->bloque_cuatro, 2, 1), 1); 
+        $validez += self::evaluar(substr($respuestas[7]->bloque_cinco, 0, 1), 1); 
+        $validez += self::evaluar(substr($respuestas[7]->bloque_cinco, 1, 1), 2); 
+        $validez += self::evaluar(substr($respuestas[7]->bloque_cinco, 2, 1), 2); 
+        $validez += self::evaluar(substr($respuestas[7]->bloque_trece, 0, 1), 2); 
+        $validez += self::evaluar(substr($respuestas[7]->bloque_trece, 2, 1), 2); 
+        //Columna 4
+        $validez += self::evaluar(substr($respuestas[8]->bloque_ocho, 0, 1), 1); 
+        $validez += self::evaluar(substr($respuestas[8]->bloque_ocho, 1, 1), 2); 
+        $validez += self::evaluar(substr($respuestas[8]->bloque_ocho, 2, 1), 2);
+        //Columna 3
+        $validez += self::evaluar(substr($respuestas[9]->bloque_cinco, 0, 1), 1); 
+        $validez += self::evaluar(substr($respuestas[9]->bloque_cinco, 1, 1), 3);
+        $validez += self::evaluar(substr($respuestas[9]->bloque_cinco, 2, 1), 2);
+        $validez += self::evaluar(substr($respuestas[9]->bloque_once, 0, 1), 1); 
+        $validez += self::evaluar(substr($respuestas[9]->bloque_once, 1, 1), 2);
+        $validez += self::evaluar(substr($respuestas[9]->bloque_once, 2, 1), 2);
+        //Columna 2
+        $validez += self::evaluar(substr($respuestas[10]->bloque_ocho, 0, 1), 2); 
+        $validez += self::evaluar(substr($respuestas[10]->bloque_ocho, 1, 1), 1);
+        $validez += self::evaluar(substr($respuestas[10]->bloque_ocho, 2, 1), 2);
+        //Columna 1
+        $validez += self::evaluar(substr($respuestas[11]->bloque_uno, 0, 1), 3);
+        $validez += self::evaluar(substr($respuestas[11]->bloque_uno, 1, 1), 2);
+        $validez += self::evaluar(substr($respuestas[11]->bloque_uno, 2, 1), 1);
+        $validez += self::evaluar(substr($respuestas[11]->bloque_tres, 0, 1), 2);
+        $validez += self::evaluar(substr($respuestas[11]->bloque_tres, 1, 1), 1);
+        $validez += self::evaluar(substr($respuestas[11]->bloque_tres, 2, 1), 3);
         return $validez;
+    }
+    public function evaluar($respuesta, $op)
+    {
+        $puntaje = 0;
+        switch ($op) {
+            case 1: // tiene -
+                if ($respuesta == '-')
+                    $puntaje++;
+                break;
+            case 2: // tiene +
+                if ($respuesta == '+')
+                    $puntaje++;
+                break;
+            case 3: // tiene + o -
+                if ($respuesta == '-' ||  $respuesta == '+')
+                    $puntaje++;
+                break;
+        }
+        return $puntaje;
     }
 }
